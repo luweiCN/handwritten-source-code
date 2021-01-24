7e4c21a69056a8ae11c2da00eb7e8219efea96f7
luwei <1280380446@qq.com>
2021-01-24 23:31:48 +0800

    手写Promise[未完成]

--- src/my-promise/my-promise.js
+++ src/my-promise/my-promise.js
@@ -1,9 +1,111 @@
-const FULLFILLED = "FULLFILLED";
-const REJUCTED = "REJUCTED";
+const FULFILLED = "FULFILLED";
+const REJECTED = "REJECTED";
 const PENDING = "PENDING";
 
 function MyPromise(executor) {
-    let status = PENDING;
+    this.status = PENDING;
+    this.onResolveHandlers = [];
+    this.onRejectHandlers = [];
+    this.value = undefined;
+    this.reason = undefined;
+    const resolve = (value) => {
+        try {
+            if (this.status === PENDING) {
+                this.status = FULFILLED;
+                this.value = value;
+                this.onResolveHandlers.forEach((handler) =>
+                    handler(this.value)
+                );
+            }
+        } catch (e) {
+            this.status = REJECTED;
+            this.reason = e;
+            this.onRejectHandlers.forEach((handler) => handler(this.reason));
+        }
+    };
+    const reject = (e) => {
+        try {
+            if (this.status === PENDING) {
+                this.status = REJECTED;
+                this.reason = e;
+                this.onRejectHandlers.forEach((handler) =>
+                    handler(this.reason)
+                );
+            }
+        } catch (e) {
+            this.status = REJECTED;
+            this.reason = e;
+            this.onRejectHandlers.forEach((handler) => handler(this.reason));
+        }
+    };
+    executor(resolve, reject);
 }
+
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+        if (this.status === PENDING) {
+            this.onResolveHandlers.push(function (value) {
+                queueMicrotask(() => {
+                    try {
+                        const res =
+                            typeof onFulfilled === "function"
+                                ? onFulfilled(value)
+                                : (value) => value;
+
+                        resolve(res);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+
+            this.onRejectHandlers.push(function (reason) {
+                queueMicrotask(() => {
+                    try {
+                        const res =
+                            typeof onRejected === "function"
+                                ? onRejected(reason)
+                                : (reason) => {
+                                      throw reason;
+                                  };
+                        resolve(res);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+        } else if (this.status === FULFILLED) {
+            queueMicrotask(() => {
+                try {
+                    const res =
+                        typeof onFulfilled === "function"
+                            ? onFulfilled(this.value)
+                            : () => this.value;
+                    resolve(res);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        } else {
+            queueMicrotask(() => {
+                try {
+                    const res =
+                        typeof onRejected === "function"
+                            ? onRejected(this.reason)
+                            : () => {
+                                  throw this.reason;
+                              };
+                    resolve(res);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        }
+    });
+};
+
+MyPromise.prototype.catch = function (onRejected) {
+    return this.then(undefined, onRejected);
+};
 
 module.exports = MyPromise;
